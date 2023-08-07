09abf6b4b1c09f3894ca8fc131276a1e8ed747d3
jyk7813 <130989673+jyk7813@users.noreply.github.com>
2023-08-07 11:01:32 +0900

    firstcommit

---
+++ src/main/java/kr/com/greenart/sdmate/pjsdmate/PjsdmateApplication.java
@@ -0,0 +1,13 @@
+package kr.com.greenart.sdmate.pjsdmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjsdmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjsdmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/com/greenart/sdmate/pjsdmate/PjsdmateApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.com.greenart.sdmate.pjsdmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjsdmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
