40b4c861ebc395e09ee86857181b151757581a25
jyk7813 <130989673+jyk7813@users.noreply.github.com>
2023-08-08 01:12:08 +0900

    loginpage 붙임 커밋해줘

--- src/main/java/kr/com/greenart/sdmate/pjsdmate/controller/MemberController.java
+++ src/main/java/kr/com/greenart/sdmate/pjsdmate/controller/MemberController.java
@@ -5,8 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
+    @GetMapping("/")
+    public String start(){
+        return "start";
+    }
     @GetMapping("/main")
     public String main() {
         return "main";
+    }
+
+    @GetMapping("/answer")
+    public String answer() {
+        return "answer";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
