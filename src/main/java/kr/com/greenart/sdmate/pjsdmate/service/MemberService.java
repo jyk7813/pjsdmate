a2725f3125cf91469635f7b697689fab55539866
juhwan <zndntn66@gmail.com>
2023-08-08 17:47:04 +0900

    로그인 로직 아마 끝났을거에요 controller 에서 연결만 해주세요

--- src/main/java/kr/com/greenart/sdmate/pjsdmate/service/MemberService.java
+++ src/main/java/kr/com/greenart/sdmate/pjsdmate/service/MemberService.java
@@ -5,20 +5,66 @@ import kr.com.greenart.sdmate.pjsdmate.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class MemberService {
-private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+
+    private final ValidatorMember validatorMember;
+
+
+
     @Autowired
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository, ValidatorMember validatorMember, ValidatorMember validatorMember1) {
         this.memberRepository = memberRepository;
+        this.validatorMember = validatorMember1;
     }
     public Member getMemberByPK(int member_no){
         Member member = memberRepository.findBymemberNo(member_no).get();
 
         return member;
     }
+    //id 를 받아서 멤버 테이블에서 조회하고 적합성 검사를 한뒤에
+    public Integer Login(String id,String pass){
 
 
+        // 레포지토리에서 값 뺴오기
+        Optional<Member> optionalMember = memberRepository.findById(id);
+        Member member = null;
+        // 값이 존재하고 비밀번호가 같다면 멤버를 반환 없으면 통과해서 null 을 반환
+        //
+        if(optionalMember.isPresent()){
+            Member checkMember = optionalMember.get();
+            if(checkMember.getPwd().equals(pass)){
+                member = checkMember;
+                return member.getMemberNo();
+            }
+        }
+        return null;
+    }
+
+    public List<String> validate(String id, String pass){
+           Map<String,String> map = validatorMember.validate(id,pass);
+           List<String> list = new ArrayList<>();
+        for (Map.Entry<String, String> entry : map.entrySet()){
+            list.add(entry.getValue());
+        }
+            return list;
+    }
+
+    public void join(Member member){
+
+        memberRepository.save(member);
+    }
+
+
+    // member 객체 JSon 파싱용 혹시 필요할까봐 남겨 놨습니다.
+//    private Member parseObj(String json) throws JsonProcessingException {
+//        Member member = objectMapper.readValue(json, Member.class);
+//        return member;
+//    }
 }
--- src/test/java/kr/com/greenart/sdmate/pjsdmate/service/MemberServiceTest.java
+++ src/test/java/kr/com/greenart/sdmate/pjsdmate/service/MemberServiceTest.java
@@ -9,6 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MemberServiceTest {
@@ -22,9 +26,23 @@ class MemberServiceTest {
 //    }
 
     @Test
-    void 찿기(){
+    void 로그인(){
+        // 로그인 되면 1 번 pk 유저랑 같음
+        //given
+        Integer current = memberService.Login("test","test1234");
+        Integer missID = memberService.Login("아이디 오타","test1234");
+        Integer missPass = memberService.Login("test","비밀번호 오타");
+        //then
+        Assertions.assertEquals(current,memberService.getMemberByPK(1).getMemberNo());
+        Assertions.assertNull(missID);
+        Assertions.assertNull(missID);
+    }
+    @Test
+    void 정규식_체크(){
+        String id = "test";
+        String password = "123 ";
+        List<String> validate = memberService.validate(id, password);
+        System.out.print(validate);
 
-        Member member = memberService.getMemberByPK(1);
-        System.out.print(member.getName());
     }
 }
