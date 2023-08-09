2c1c2f6944717f39719caf923503d39ea58a48ba
yxxn.dev <yxxn.dev@gmail.com>
2023-08-09 11:44:26 +0900

    main (일반) 견적카드 반복 추가 기능 구현

--- src/main/java/kr/com/greenart/sdmate/pjsdmate/controller/MemberController.java
+++ src/main/java/kr/com/greenart/sdmate/pjsdmate/controller/MemberController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -50,18 +52,23 @@ public class MemberController {
 
 
     @GetMapping("/mainyxxn")
-    public String mainyxxn(HttpSession session) {
-        mainpageCard card = new mainpageCard();
-        card.setSum(3000000);
-        card.setBusinessName("테스트사업");
-        card.setDealCnt(48);
-        card.setRating((long) 2.8);
-        card.setPlannerPk(1);
-        card.setPlannerImg(null);
-        card.setReviewCnt(3);
-        System.out.println(card);
-        session.setAttribute("card", card);
+    public String mainyxxn(Model model) {
+        List<mainpageCard> cardList = new ArrayList<>();
 
-        return "mainyxxn";
+        for(int i = 1; i < 4; i++){
+            mainpageCard card = new mainpageCard();
+            card.setSum(3000000+i);
+            card.setBusinessName("테스트사업"+i);
+            card.setDealCnt(48+i);
+            card.setRating((long) 2.8+i);
+            card.setPlannerPk(1+i);
+            card.setPlannerImg(null);
+            card.setReviewCnt(3+i);
+            System.out.println(card);
+            cardList.add(card);
+        }
+        model.addAttribute("cardList", cardList);
+
+        return "main";
     }
 }
