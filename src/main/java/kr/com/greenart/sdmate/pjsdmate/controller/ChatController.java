ee4d26c356a79f210463c4ee46d8da67d4cec297
hojae <hj981114@gmail.com>
2023-08-16 00:08:09 +0900

    pull 용

--- src/main/java/kr/com/greenart/sdmate/pjsdmate/controller/ChatController.java
+++ src/main/java/kr/com/greenart/sdmate/pjsdmate/controller/ChatController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/chat")
 public class ChatController {
 
 
@@ -17,3 +18,4 @@ public class ChatController {
         return "Mchat";
     }
 }
+
