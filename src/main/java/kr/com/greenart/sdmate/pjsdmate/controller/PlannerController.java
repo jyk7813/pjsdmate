1850181df4c41a3bb59271ac624935394162a3dd
hojae <hj981114@gmail.com>
2023-08-16 01:21:05 +0900

    디테일 잡고 있습니다

--- src/main/java/kr/com/greenart/sdmate/pjsdmate/controller/PlannerController.java
+++ src/main/java/kr/com/greenart/sdmate/pjsdmate/controller/PlannerController.java
@@ -48,11 +48,8 @@ public class PlannerController {
     @GetMapping("/main")
     public String mainplanner(Model model, HttpSession session) throws IOException {
         Planner planner = (Planner) session.getAttribute("planner");
-        System.out.println("플래너 pk : " + planner.getPlannerNo());
         List<PlannermainpageCard> card = plannerMainPageService.returnPlannerMainCard(planner.getPlannerNo());
-        System.out.println("requirmentPk:"+card.get(0).getRequirementPk());
         model.addAttribute("card", card);
-        System.out.println(card);
         return "mainplanner";
     }
 
